acb30042f9be7277347a20e63a464604d07bf65a
serofuku <166649317+serofuku@users.noreply.github.com>
2026-05-24 00:11:03 +0800

    Update os.c

--- port-esp32s3/main/os.c
+++ port-esp32s3/main/os.c
@@ -94,14 +94,20 @@ void lcdWriteFB(uint8_t *buf, int len) {
 }
 
 void lcdInit() {
+  // Backlight on
+  gpio_set_direction(PIN_LCD_BCKL, GPIO_MODE_OUTPUT);
+  gpio_set_level(PIN_LCD_BCKL, 1);
+
+  // Reset
   gpio_set_direction(PIN_SYS_RSTN, GPIO_MODE_OUTPUT);
-  gpio_set_level(PIN_LCD_CS, 1);
   gpio_set_level(PIN_SYS_RSTN, 0);
-  delayMS(500);
+  delayMS(100);
   gpio_set_level(PIN_SYS_RSTN, 1);
-  gpio_set_direction(PIN_LCD_CS, GPIO_MODE_OUTPUT);
-  gpio_set_level(PIN_LCD_DC, 1);
+  delayMS(100);
+
   gpio_set_direction(PIN_LCD_DC, GPIO_MODE_OUTPUT);
+  gpio_set_level(PIN_LCD_DC, 1);
+
   static const spi_bus_config_t buscfg = {
       .miso_io_num = PIN_SPI0_MISO,
       .mosi_io_num = PIN_SPI0_MOSI,
@@ -111,98 +117,47 @@ void lcdInit() {
       .max_transfer_sz = 120000,
   };
   ESP_ERROR_CHECK(spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO));
-  ESP_LOGI("HAL", "SPI2 initialized");
-  static const spi_device_interface_config_t devcfg = {.clock_speed_hz = 60000000,
-                                          .mode = 0,
-                                          .spics_io_num = -1,
-                                          .queue_size = 7};
+
+  static const spi_device_interface_config_t devcfg = {
+      .clock_speed_hz = 40000000,
+      .mode = 0,
+      .spics_io_num = -1,
+      .queue_size = 7
+  };
   ESP_ERROR_CHECK(spi_bus_add_device(SPI2_HOST, &devcfg, &spiDev0));
 
-  ESP_LOGI("HAL", "Device Added");
+  // ILI9341 init sequence
+  lcdCmd8(0x01); delayMS(50);   // Software reset
+  lcdCmd8(0x11); delayMS(120);  // Sleep out
 
-  //************* Start Initial Sequence **********//
-  lcdCmd8(0x11);  // Sleep out
-  delayMS(120);   // Delay 120ms
-  //************* Start Initial Sequence **********//
-  lcdCmd8(0x36);
-  if (USE_HORIZONTAL == 0)
-    lcdDat8(0x00);
-  else if (USE_HORIZONTAL == 1)
-    lcdDat8(0xC0);
-  else if (USE_HORIZONTAL == 2)
-    lcdDat8(0x70);
-  else
-    lcdDat8(0xA0);
-
-  lcdCmd8(0x3A);
-  lcdDat8(0x05);
-
-  lcdCmd8(0xB2);
-  lcdDat8(0x0C);
-  lcdDat8(0x0C);
-  lcdDat8(0x00);
-  lcdDat8(0x33);
-  lcdDat8(0x33);
-
-  lcdCmd8(0xB7);
-  lcdDat8(0x35);
-
-  lcdCmd8(0xBB);
-  lcdDat8(0x19);
-
-  lcdCmd8(0xC0);
-  lcdDat8(0x2C);
-
-  lcdCmd8(0xC2);
-  lcdDat8(0x01);
-
-  lcdCmd8(0xC3);
-  lcdDat8(0x12);
-
-  lcdCmd8(0xC4);
-  lcdDat8(0x20);
-
-  lcdCmd8(0xC6);
-  lcdDat8(0x0F);
-
-  lcdCmd8(0xD0);
-  lcdDat8(0xA4);
-  lcdDat8(0xA1);
-
+  lcdCmd8(0xCF); lcdDat8(0x00); lcdDat8(0xC3); lcdDat8(0x30);
+  lcdCmd8(0xED); lcdDat8(0x64); lcdDat8(0x03); lcdDat8(0x12); lcdDat8(0x81);
+  lcdCmd8(0xE8); lcdDat8(0x85); lcdDat8(0x00); lcdDat8(0x78);
+  lcdCmd8(0xCB); lcdDat8(0x39); lcdDat8(0x2C); lcdDat8(0x00); lcdDat8(0x34); lcdDat8(0x02);
+  lcdCmd8(0xF7); lcdDat8(0x20);
+  lcdCmd8(0xEA); lcdDat8(0x00); lcdDat8(0x00);
+  lcdCmd8(0xC0); lcdDat8(0x1B);
+  lcdCmd8(0xC1); lcdDat8(0x12);
+  lcdCmd8(0xC5); lcdDat8(0x32); lcdDat8(0x3C);
+  lcdCmd8(0xC7); lcdDat8(0x91);
+  lcdCmd8(0x36); lcdDat8(0x08);
+  lcdCmd8(0x3A); lcdDat8(0x55);
+  lcdCmd8(0xB1); lcdDat8(0x00); lcdDat8(0x10);
+  lcdCmd8(0xB6); lcdDat8(0x0A); lcdDat8(0xA2);
+  lcdCmd8(0xF2); lcdDat8(0x00);
+  lcdCmd8(0x26); lcdDat8(0x01);
   lcdCmd8(0xE0);
-  lcdDat8(0xD0);
-  lcdDat8(0x04);
-  lcdDat8(0x0D);
-  lcdDat8(0x11);
-  lcdDat8(0x13);
-  lcdDat8(0x2B);
-  lcdDat8(0x3F);
-  lcdDat8(0x54);
-  lcdDat8(0x4C);
-  lcdDat8(0x18);
-  lcdDat8(0x0D);
-  lcdDat8(0x0B);
-  lcdDat8(0x1F);
-  lcdDat8(0x23);
-
+    lcdDat8(0x0F); lcdDat8(0x31); lcdDat8(0x2B); lcdDat8(0x0C);
+    lcdDat8(0x0E); lcdDat8(0x08); lcdDat8(0x4E); lcdDat8(0xF1);
+    lcdDat8(0x37); lcdDat8(0x07); lcdDat8(0x10); lcdDat8(0x03);
+    lcdDat8(0x0E); lcdDat8(0x09); lcdDat8(0x00);
   lcdCmd8(0xE1);
-  lcdDat8(0xD0);
-  lcdDat8(0x04);
-  lcdDat8(0x0C);
-  lcdDat8(0x11);
-  lcdDat8(0x13);
-  lcdDat8(0x2C);
-  lcdDat8(0x3F);
-  lcdDat8(0x44);
-  lcdDat8(0x51);
-  lcdDat8(0x2F);
-  lcdDat8(0x1F);
-  lcdDat8(0x1F);
-  lcdDat8(0x20);
-  lcdDat8(0x23);
-  lcdCmd8(0x21);
-
-  lcdCmd8(0x29);
+    lcdDat8(0x00); lcdDat8(0x0E); lcdDat8(0x14); lcdDat8(0x03);
+    lcdDat8(0x11); lcdDat8(0x07); lcdDat8(0x31); lcdDat8(0xC1);
+    lcdDat8(0x48); lcdDat8(0x08); lcdDat8(0x0F); lcdDat8(0x0C);
+    lcdDat8(0x31); lcdDat8(0x36); lcdDat8(0x0F);
+  lcdCmd8(0x11); delayMS(120);
+  lcdCmd8(0x29); // Display on
 }
 
 
